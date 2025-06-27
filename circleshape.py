f74449ac9cf3f503a1604fa9fefbc2c539a94ec0
Wyrda <guillermo94silva@gmail.com>
2025-06-27 10:18:24 -0300

    C: Creacion de circleshape.py

---
+++ circleshape.py
@@ -0,0 +1,21 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "container"):
+            super().__init__(self.container)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+
+    def draw(self, screen):
+        # Sub_classes should override
+        pass
+
+    def update(self, dt):
+        # sub_classes should override
+        pass
